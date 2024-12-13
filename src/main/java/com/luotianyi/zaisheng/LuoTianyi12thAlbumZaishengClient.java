2aab6001274e24a88de8335a8cf832ba0128dff8
honggggv <hongwanzhen0000@outlook.com>
2024-12-13 10:05:46 +0800

    Initial commit

---
+++ src/main/java/com/luotianyi/zaisheng/LuoTianyi12thAlbumZaishengClient.java
@@ -0,0 +1,12 @@
+package com.luotianyi.zaisheng;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import javax.swing.event.CaretListener;
+
+public class LuoTianyi12thAlbumZaishengClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/luotianyi/zaisheng/LuoTianyi12thAlbumZaishengDataGenerator.java
@@ -0,0 +1,11 @@
+package com.luotianyi.zaisheng;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LuoTianyi12thAlbumZaishengDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
